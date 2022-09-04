9b7e9e22fe1b76d65789b54288cdcac778e0cf43
Jeremy Houser <foxattack3@gmail.com>
2022-09-04 02:34:18 +0000

    Added rollOption for crowbar to automate -2 penalty, modified Force Open feat, crowbar, and levered crowbar to remove this penalty as appropriate

--- src/module/system/action-macros/athletics/force-open.ts
+++ src/module/system/action-macros/athletics/force-open.ts
@@ -1,14 +1,26 @@
 import { ActionMacroHelpers, SkillActionOptions } from "..";
+import { ModifierPF2e } from "@actor/modifiers";
 
 export function forceOpen(options: SkillActionOptions) {
     const { checkType, property, stat, subtitle } = ActionMacroHelpers.resolveStat(options?.skill ?? "athletics");
+
+    const modifiers = (options.modifiers ?? []).concat(
+        new ModifierPF2e({
+            label: "PF2E.Actions.ForceOpen.Modifier.NoCrowbar",
+            modifier: -2,
+            predicate: {
+                not: ["crowbar-in-possession"],
+            },
+        })
+    );
+
     ActionMacroHelpers.simpleRollActionCheck({
         actors: options.actors,
         statName: property,
         actionGlyph: options.glyph ?? "A",
         title: "PF2E.Actions.ForceOpen.Title",
         subtitle,
-        modifiers: options.modifiers,
+        modifiers,
         rollOptions: ["all", checkType, stat, "action:force-open"],
         extraOptions: ["action:force-open"],
         traits: ["attack"],
